3877f64898fb974aa80e052940eb31df1ddc4b7d
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-10-06 15:49:45 +0200

    created vue routes

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,25 +2,20 @@
     <div>
         <AppHeader></AppHeader>
         <div class="container">
-            <HomePage></HomePage>
-            <ContactsPage></ContactsPage>
-            <AboutPage></AboutPage>
+            <router-view></router-view>
         </div>
     </div>
 </template>
 
 <script>
-import HomePage from './pages/HomePage.vue';
+
 import AppHeader from './AppHeader.vue';
-import ContactsPage from './pages/ContactsPage.vue';
-import AboutPage from './pages/AboutPage.vue';
+
 export default {
     name: 'App',
     components: {
-    AppHeader,
-    HomePage,
-    ContactsPage,
-    AboutPage
-}
+        AppHeader,
+    
+    }
 };
 </script>
--- resources/js/front.js
+++ resources/js/front.js
@@ -4,13 +4,15 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-
+import router from './router';
 import App from './components/App.vue';
 
 const root = new Vue({
+    router,
     el: '#root',
     components: {
         App,
     },
     render: h => h(App)
 });
+
